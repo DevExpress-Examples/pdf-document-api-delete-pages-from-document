678f0102efbbf4eefe833f9de88f1de83438dacb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PdfDeletePageExample/Program.cs
@@ -0,0 +1,18 @@
+﻿#region #Reference
+using DevExpress.Pdf;
+// ...
+#endregion #Reference
+
+namespace PdfDeletePageExample {
+#region #Code
+class Program {
+    static void Main(string[] args) {
+        PdfDocumentProcessor pdfDocumentProcessor = new PdfDocumentProcessor();
+        pdfDocumentProcessor.LoadDocument("..\\..\\docs\\TextDelete.pdf");
+        for (int i = pdfDocumentProcessor.Document.Pages.Count / 2; i >= 0; i--)
+            pdfDocumentProcessor.DeletePage(i * 2 + 1);
+        pdfDocumentProcessor.SaveDocument("..\\..\\docs\\Deleted.pdf");
+    }
+}
+#endregion #Code
+}
---
+++ VB/PdfDeletePageExample/Program.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+#Region "#Reference"
+Imports DevExpress.Pdf
+' ...
+#End Region ' #Reference
+
+Namespace PdfDeletePageExample
+#Region "#Code"
+Friend Class Program
+	Shared Sub Main(ByVal args() As String)
+		Dim pdfDocumentProcessor As New PdfDocumentProcessor()
+		pdfDocumentProcessor.LoadDocument("..\..\docs\TextDelete.pdf")
+            For i As Integer = Convert.ToInt16(pdfDocumentProcessor.Document.Pages.Count / 2) To 0 Step -1
+                pdfDocumentProcessor.DeletePage(i * 2 + 1)
+            Next i
+		pdfDocumentProcessor.SaveDocument("..\..\docs\Deleted.pdf")
+	End Sub
+End Class
+#End Region ' #Code
+End Namespace
