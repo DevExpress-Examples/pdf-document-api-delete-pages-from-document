bc4a93b050036049f31c9dee7bab096e33c41a9c
geumjaLee <polina.fedorova@devexpress.com>
2020-10-12 13:33:43 +0300

    update code snippet

--- CS/PdfDeletePageExample/Program.cs
+++ CS/PdfDeletePageExample/Program.cs
@@ -7,11 +7,16 @@ using DevExpress.Pdf;
 namespace PdfDeletePageExample {
     #region #Code
     class Program {
-        static void Main(string[] args) {
-            using (PdfDocumentProcessor pdfDocumentProcessor = new PdfDocumentProcessor()) {
+        static void Main(string[] args) 
+        {
+            using (PdfDocumentProcessor pdfDocumentProcessor = new PdfDocumentProcessor()) 
+            {
                 pdfDocumentProcessor.LoadDocument("..\\..\\docs\\TextDelete.pdf");
-                for (int i = pdfDocumentProcessor.Document.Pages.Count / 2; i >= 0; i--)
-                    pdfDocumentProcessor.DeletePage(i * 2 + 1);
+                for (int i = pdfDocumentProcessor.Document.Pages.Count-1; i >= 0; i--)
+                    if (i % 2 != 0)
+                    {
+                        pdfDocumentProcessor.DeletePage(i);
+                    }
                 pdfDocumentProcessor.SaveDocument("..\\..\\docs\\Deleted.pdf");
             }
         }
--- VB/PdfDeletePageExample/Program.vb
+++ VB/PdfDeletePageExample/Program.vb
@@ -4,17 +4,19 @@ Imports DevExpress.Pdf
 #End Region ' #Reference
 
 Namespace PdfDeletePageExample
-    #Region "#Code"
-    Friend Class Program
-        Shared Sub Main(ByVal args() As String)
-            Using pdfDocumentProcessor As New PdfDocumentProcessor()
-                pdfDocumentProcessor.LoadDocument("..\..\docs\TextDelete.pdf")
-                For i As Integer = pdfDocumentProcessor.Document.Pages.Count \ 2 To 0 Step -1
-                    pdfDocumentProcessor.DeletePage(i * 2 + 1)
-                Next i
-                pdfDocumentProcessor.SaveDocument("..\..\docs\Deleted.pdf")
-            End Using
-        End Sub
-    End Class
-    #End Region ' #Code
+	#Region "#Code"
+	Friend Class Program
+		Shared Sub Main(ByVal args() As String)
+			Using pdfDocumentProcessor As New PdfDocumentProcessor()
+				pdfDocumentProcessor.LoadDocument("..\..\docs\TextDelete.pdf")
+				For i As Integer = pdfDocumentProcessor.Document.Pages.Count-1 To 0 Step -1
+					If i Mod 2 <> 0 Then
+						pdfDocumentProcessor.DeletePage(i)
+					End If
+				Next i
+				pdfDocumentProcessor.SaveDocument("..\..\docs\Deleted.pdf")
+			End Using
+		End Sub
+	End Class
+	#End Region ' #Code
 End Namespace
