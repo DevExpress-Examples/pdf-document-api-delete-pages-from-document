0ef404004645404a92092b4f87029c91ba510602
Poline Fedorova <polina.fedorova@devexpress.com>
2020-10-13 17:24:24 +0300

    Update Program.cs

--- CS/PdfDeletePageExample/Program.cs
+++ CS/PdfDeletePageExample/Program.cs
@@ -12,7 +12,7 @@ namespace PdfDeletePageExample {
             using (PdfDocumentProcessor pdfDocumentProcessor = new PdfDocumentProcessor()) 
             {
                 pdfDocumentProcessor.LoadDocument("..\\..\\docs\\TextDelete.pdf");
-                for (int i = pdfDocumentProcessor.Document.Pages.Count-1; i >= 0; i--)
+                for (int i = pdfDocumentProcessor.Document.Pages.Count-1; i > 0; i--)
                     if (i % 2 != 0)
                     {
                         pdfDocumentProcessor.DeletePage(i);
