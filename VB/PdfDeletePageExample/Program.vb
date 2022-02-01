924fe3fc11ee734220e59b1190fa710d6d15510b
DevExpressExampleBot <fakeEmail>
2022-02-01 18:13:54 +0300

    Source auto update [skip ci]

--- VB/PdfDeletePageExample/Program.vb
+++ VB/PdfDeletePageExample/Program.vb
@@ -1,22 +1,25 @@
-﻿#Region "#Reference"
+#Region "#Reference"
 Imports DevExpress.Pdf
-' ...
-#End Region ' #Reference
 
+' ...
+#End Region  ' #Reference
 Namespace PdfDeletePageExample
-	#Region "#Code"
-	Friend Class Program
-		Shared Sub Main(ByVal args() As String)
-			Using pdfDocumentProcessor As New PdfDocumentProcessor()
-				pdfDocumentProcessor.LoadDocument("..\..\docs\TextDelete.pdf")
-				For i As Integer = pdfDocumentProcessor.Document.Pages.Count To 1 Step -1
-					If i Mod 2 <> 0 Then
-						pdfDocumentProcessor.DeletePage(i)
-					End If
-				Next i
-				pdfDocumentProcessor.SaveDocument("..\..\docs\Deleted.pdf")
-			End Using
-		End Sub
-	End Class
-	#End Region ' #Code
+
+#Region "#Code"
+    Friend Class Program
+
+        Shared Sub Main(ByVal args As String())
+            Using pdfDocumentProcessor As PdfDocumentProcessor = New PdfDocumentProcessor()
+                pdfDocumentProcessor.LoadDocument("..\..\docs\TextDelete.pdf")
+                For i As Integer = pdfDocumentProcessor.Document.Pages.Count To 0 + 1 Step -1
+                    If i Mod 2 <> 0 Then
+                        pdfDocumentProcessor.DeletePage(i)
+                    End If
+                Next
+
+                pdfDocumentProcessor.SaveDocument("..\..\docs\Deleted.pdf")
+            End Using
+        End Sub
+    End Class
+#End Region  ' #Code
 End Namespace
